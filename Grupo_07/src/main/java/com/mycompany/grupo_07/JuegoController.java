31cdd02a3aaf743912b8310d510d4f1d721651a1
Jazarumag <118693522+Jazarumag@users.noreply.github.com>
2024-08-17 22:23:49 -0500

    A

--- Grupo_07/src/main/java/com/mycompany/grupo_07/JuegoController.java
+++ Grupo_07/src/main/java/com/mycompany/grupo_07/JuegoController.java
@@ -5,7 +5,10 @@
 package com.mycompany.grupo_07;
 
 import java.io.IOException;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 /**
  *
@@ -16,5 +19,15 @@ public class JuegoController {
     private void switchToPrincipal() throws IOException {
         App.setRoot("menuprincipal");
     }
-    
+    @FXML
+    private void switchToMenu() throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmación de Regreso");
+        alert.setHeaderText(null);
+        alert.setContentText("¿Estás seguro de que deseas salir?");
+        ButtonType result = alert.showAndWait().orElse(ButtonType.CANCEL);
+        if (result == ButtonType.OK) {
+            App.setRoot("menuprincipal");
+        }
+    }
 }
